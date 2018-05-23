3f65bd23af07126593e73c74516f685eeb73fde5
hz1411962 <hz1411962@huize.com>
2018-05-23 21:40:02 +0800

    测试get 没有成功

--- server/controllers/demo.js
+++ server/controllers/demo.js
@@ -1,5 +1,26 @@
+var http = require("http")
+var url = require("url")
+var util = require("util")
+var querystring = require("querystring")
+
 module.exports = ctx => {
   ctx.state.data = {
-    msg: 'Hello jiurong'
+    msg: "jiurong"
   }
+  // http.get('http://v.juhe.cn/weixin/query?key=f16af393a63364b729fd81ed9fdd4b7d&pno=1&ps=10', function (response) {
+  //   var body = [];
+  //   console.log(response.statusCode);
+  //   console.log(response.headers);
+  //   console.log(response);
+  //   response.on('data', function (chunk) {
+  //     body.push(chunk);
+  //   });
+  //   response.on('end', function () {
+  //     body = Buffer.concat(body);
+  //     ctx.state.data = {
+  //       msg: body
+  //     }
+  //     console.log(body.toString());
+  //   });
+  // });
 }
